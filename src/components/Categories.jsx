7ddf333e489f4641367a69437a9197269a773e68
Yevgeniy Shatalin <shatalin_ei@mail.ru>
2022-10-22 21:06:59 +0600

    adding logic to highlight the active category and refactoring the output of categories from the array

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 
 function Categories() {
+    const [activeIndex, setActiveIndex] = React.useState(0)
+
+    const onClickCategory = (index) => {
+        setActiveIndex(index)
+    }
+
+    const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                {categories.map((category, index) => {
+                    return  <li className={activeIndex === index ? "active" : ''} onClick={() => onClickCategory(index)}>{category}</li>
+                })}
             </ul>
         </div>
     )
