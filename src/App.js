2ecf7fb3a0e908240c10985684abc9071547c004
Yevgeniy Shatalin <shatalin_ei@mail.ru>
2022-10-23 22:12:32 +0600

    added React Router library, configured pagination

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { Routes, Route} from "react-router-dom";
 
 import Header from "./components/Header";
-
 import './scss/app.scss'
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
 
 function App() {
 
     return (
         <div className="wrapper">
-            <Header/>
+           <Header/>
             <div className="content">
                 <div className="container">
-                    <NotFound/>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/cart" element={<Cart />} />
+                        <Route path="*" element={ <NotFound/>} />
+                    </Routes>
                 </div>
             </div>
         </div>
