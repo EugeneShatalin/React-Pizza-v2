53b264d16b4a6915eac18c04df249c42e0e46ca3
Yevgeniy Shatalin <shatalin_ei@mail.ru>
2022-10-23 14:37:31 +0600

    wrote logic for sorting popup window

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 function Sort() {
+
+    const [open, setOpen] = React.useState(false)
+    const [selected, setSelected] = React.useState(0)
+    const list = ['популярности', 'цене', 'алфавиту']
+
+    const onClickListItem = (i) => {
+        setSelected(i)
+        setOpen(!open)
+    }
+
     return (
         <div className="sort">
             <div className="sort__label">
@@ -17,15 +27,21 @@ function Sort() {
                     />
                 </svg>
                 <b>Сортировка по:</b>
-                <span>популярности</span>
+                <span onClick={() => setOpen(!open)}>{list[selected]}</span>
             </div>
-            <div className="sort__popup">
-                <ul>
-                    <li className="active">популярности</li>
-                    <li>цене</li>
-                    <li>алфавиту</li>
-                </ul>
-            </div>
+            {
+                open && (<div className="sort__popup">
+                    <ul>
+                        {list.map((name, index) => (
+                            <li key={index}
+                                className={selected === index ? "active" : ''}
+                                onClick={() => onClickListItem(index)}>
+                                {name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>)
+            }
         </div>
     )
 }
