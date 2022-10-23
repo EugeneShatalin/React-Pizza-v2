d9313818192f7dc21d603a6e8f80f128bed9ffcb
Yevgeniy Shatalin <shatalin_ei@mail.ru>
2022-10-23 21:12:38 +0600

    divided the application into pages, moved the logic of the main page to the Home component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
 import React from "react";
 
 import Header from "./components/Header";
-import Categories from "./components/Categories";
-import Sort from "./components/Sort";
-import Index from "./components/PizzaBlock";
 
 import './scss/app.scss'
-import Skeleton from "./components/PizzaBlock/Skeleton";
-
 
 function App() {
-
-    const [pizzas, setPizzas] = React.useState([])
-    const [isLoading, setIsLoading] = React.useState(true)
-
-    React.useEffect(() => {
-        fetch('https://react-pizza.free.beeceptor.com/items')
-            .then((res) => res.json())
-            .then((arr) => {
-                setPizzas(arr)
-                setIsLoading(false)
-            })
-
-    }, [])
 
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
                 <div className="container">
-                    <div className="content__top">
-                        <Categories/>
-                        <Sort/>
-                    </div>
-                    <h2 className="content__title">Все пиццы</h2>
-                    <div className="content__items">
-                        {isLoading
-                            ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>)
-                            : pizzas.map(pizza => (<Index key={pizza.id} {...pizza}/>))
-                        }
-                    </div>
+
                 </div>
             </div>
         </div>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import Skeleton from "../components/PizzaBlock/Skeleton";
+import Index from "../components/PizzaBlock";
+
+const Home = () => {
+
+    const [pizzas, setPizzas] = React.useState([])
+    const [isLoading, setIsLoading] = React.useState(true)
+
+    React.useEffect(() => {
+        fetch('https://react-pizza.free.beeceptor.com/items')
+            .then((res) => res.json())
+            .then((arr) => {
+                setPizzas(arr)
+                setIsLoading(false)
+            })
+
+    }, [])
+
+    return (
+        <>
+            <div className="content__top">
+                <Categories/>
+                <Sort/>
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+                {isLoading
+                    ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>)
+                    : pizzas.map(pizza => (<Index key={pizza.id} {...pizza}/>))
+                }
+            </div>
+        </>
+    );
+};
+
+export default Home;
