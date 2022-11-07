40c7bc78800d66d49973a8824bb8b9b71f1f70b8
Yevgeniy Shatalin <shatalin_ei@mail.ru>
2022-11-07 21:18:32 +0600

    connected the Redux Toolkit library, saved the selected category in the Redux Toolkit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,15 +6,24 @@ import PizzaBlock from "../components/PizzaBlock/pizzaBlock";
 import Pagination from "../components/Pagination";
 import {SearchContext} from "../App";
 
+import {useDispatch, useSelector} from "react-redux";
+import {setCategoryId} from "../redux/slices/filterSlice";
+
 const Home = () => {
+    const dispatch = useDispatch()
+    const categoryId = useSelector(state => state.filter.categoryId)
 
     const {searchValue} = React.useContext(SearchContext)
 
     const [items, setItems] = React.useState([])
     const [isLoading, setIsLoading] = React.useState(true)
-    const [categoryId, setCategoryId] = React.useState(0)
     const [currentPage, setCurrentPage] = React.useState(1)
     const [sortType, setSortType] = React.useState({name: 'популярности (по убыванию)', sortProperty: 'rating'})
+
+    const onChangeCategory = (id) => {
+        dispatch(setCategoryId(id))
+    }
+
 
     React.useEffect(() => {
         setIsLoading(true)
@@ -48,7 +57,7 @@ const Home = () => {
     return (
         <div className="container">
             <div className="content__top">
-                <Categories value={categoryId} onChangeCategory={(id) => setCategoryId(id)}/>
+                <Categories value={categoryId} onChangeCategory={onChangeCategory}/>
                 <Sort value={sortType} onChangeSort={(obj) => setSortType(obj)}/>
             </div>
             <h2 className="content__title">Все пиццы</h2>
