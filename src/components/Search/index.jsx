eafdecb19c6333f6d3a9a2a29fa574d37010c008
Yevgeniy Shatalin <shatalin_ei@mail.ru>
2022-11-08 23:11:45 +0600

    added a delayed function call through the debounce method of the lodash library, to reduce requests to the server when entering the input

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import debounce from 'lodash.debounce';
 
 import styles from './Search.module.scss'
 import {SearchContext} from "../../App";
 
 const Search = () => {
-
-    const {searchValue, setSearchValue} = React.useContext(SearchContext)
+    const [value, setValue] = React.useState('')
+    const {setSearchValue} = React.useContext(SearchContext)
 
     const inputRef = React.useRef()
     const onClickClear = () => {
+        setValue('')
         setSearchValue('')
         inputRef.current.focus()
+    }
+
+    const updateSearchValue = React.useCallback(
+        debounce((str) => {
+            setSearchValue(str)
+        }, 250),
+        []
+    )
+
+    const onChangeInput = (event) => {
+        setValue(event.target.value)
+        updateSearchValue(event.target.value)
     }
 
     return (
@@ -27,7 +41,7 @@ const Search = () => {
                     d="M27.414,24.586l-5.077-5.077C23.386,17.928,24,16.035,24,14c0-5.514-4.486-10-10-10S4,8.486,4,14  s4.486,10,10,10c2.035,0,3.928-0.614,5.509-1.663l5.077,5.077c0.78,0.781,2.048,0.781,2.828,0  C28.195,26.633,28.195,25.367,27.414,24.586z M7,14c0-3.86,3.14-7,7-7s7,3.14,7,7s-3.14,7-7,7S7,17.86,7,14z"
                     id="XMLID_223_"/>
             </svg>
-            {searchValue && (
+            {value && (
                 <svg
                     onClick={onClickClear}
                     className={styles.clearIcon}
@@ -35,16 +49,17 @@ const Search = () => {
                     viewBox="0 0 48 48"
                     width="48"
                     xmlns="http://www.w3.org/2000/svg">
-                    <path d="M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z"/>
+                    <path
+                        d="M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z"/>
                     <path d="M0 0h48v48h-48z" fill="none"/>
                 </svg>
             )}
             <input
                 ref={inputRef}
-                onClick={()=>onClickClear}
-                onChange={(event) => setSearchValue(event.target.value)}
+                onClick={() => onClickClear}
+                onChange={(event) => onChangeInput(event)}
                 className={styles.input} placeholder="Поиск пиццы..."
-                value={searchValue}/>
+                value={value}/>
         </div>
 
     );
