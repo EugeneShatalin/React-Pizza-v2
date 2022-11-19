b35103896316aea92789ceb5906bbd43b15e527f
Yevgeniy Shatalin <shatalin_ei@mail.ru>
2022-11-19 22:04:05 +0600

    fixed duplication of objects with pizza parameters when adding it again

--- src/components/PizzaBlock/pizzaBlock.jsx
+++ src/components/PizzaBlock/pizzaBlock.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {addItem} from "../../redux/slices/cartSlice";
 
 function PizzaBlock({id, title, imageUrl, types, sizes, price}) {
     const dispatch = useDispatch();
+    const cartItem = useSelector(state => state.cart.items.find(obj => obj.id === id))
     const [activeType, setActiveType] = React.useState(0)
     const [activeSize, setActiveSize] = React.useState(0)
     const typeNames = ['тонкое', 'традиционное']
+
+    const addedCount = cartItem ? cartItem.count : 0
 
     const onClickType = (typeId) => {
         setActiveType(typeId)
@@ -70,7 +73,7 @@ function PizzaBlock({id, title, imageUrl, types, sizes, price}) {
                             />
                         </svg>
                         <span>Добавить</span>
-                        <i>2</i>
+                        {addedCount > 0 && <i>{addedCount}</i>}
                     </button>
                 </div>
             </div>
